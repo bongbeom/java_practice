2fe5706751f9274175fdc285279858e7663c59ff
이봉범 <bongbeom@ibongbeom-ui-MacBookPro.local>
2021-03-02 11:26:37 +0900

    java first class

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int intValue = 1;
+        char charValue = 'A';
+        double doubleValue = 5.7;
+        String strValue = "A";
+        
+        double var = (double) intValue;
+        byte var1 = (byte) intValue;
+        int var2 = (int) doubleValue;
+        char var3 = (char) strValue;
+    }
+}
